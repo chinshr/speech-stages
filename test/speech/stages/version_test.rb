05ddcc4c9b7e0cd8ebe1439688acedc73fe6c9db
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-12-27 08:51:40 -0800

    Bump version

--- test/speech/stages/version_test.rb
+++ test/speech/stages/version_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Speech::Stages::VersionTest < Test::Unit::TestCase
 
   def test_current_version
-    assert_equal "0.0.1", Speech::Stages::VERSION
+    assert_equal "0.0.2", Speech::Stages::VERSION
   end
 
 end
